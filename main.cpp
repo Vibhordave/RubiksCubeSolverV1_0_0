dc55e074d066a72ac20e1c24e6ef7fde9d37146d
Vibhordave <vibhor.dave03@gmail.com>
2023-07-03 19:18:44 +0530

    Changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,45 @@
 #include <bits/stdc++.h>
 #include "Data Structures/Cube3D.cpp"
+#include "Data Structures/CubeBitboard.cpp"
+#include "Data Structures/Cube1D.cpp"
+#include "CubeSolvers/CubeBFS.h"
+#include "CubeSolvers/CubeDFS.h"
 using namespace std;
 int main() {
-    Cube3D cube;
+
+     //DFS Solver Testing __________________________________________________________________________________________
+     cout<<"Solving with DFS ----------------------------------------------------------------"<<endl;
+     Cube3D cube;
+     cube.print();
+
+     vector<Cube3D::MOVE> shuffle_moves = cube.randomShuffleCube(6);
+     for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+     cout << "\n";
+     cube.print();
+
+     CubeDFS<Cube3D, Hash3d> dfsSolver(cube, 8);
+     vector<Cube3D::MOVE> solve_moves = dfsSolver.solve();
+
+     for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+     cout << "\n";
+     dfsSolver.rubiksCube.print();
+
+
+    //BFS Solver -----------------------------------------------------------------------------------------------------
+    cout<<"Solving with BFS ----------------------------------------------------------------"<<endl;
+    CubeBitboard cube2;
+    cube2.print();
+
+    vector<Cube3D::MOVE> shuffle_moves2 = cube2.randomShuffleCube(6);
+    for(auto move: shuffle_moves2) cout << cube2.getMove(move) << " ";
+    cout << "\n";
     cube.print();
+
+    CubeBFS<CubeBitboard, HashBitboard> bfsSolver(cube2);
+    vector<GenericCube::MOVE> solve_moves2 = bfsSolver.solve();
+
+    for(auto move: solve_moves2) cout << cube2.getMove(move) << " ";
+    cout << "\n";
+    bfsSolver.rubiksCube.print();
     return 0;
 }
